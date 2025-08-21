f026472d6f8bffe415711f8146045783f81c0c50
Victor Neves <victorneves478@gmail.com>
2025-08-21 11:58:29 -0300

    Update schemas.py

--- workout_api/contrib/schemas.py
+++ workout_api/contrib/schemas.py
@@ -1,14 +1,15 @@
 from typing import Annotated
-from pydantic import UUID4, BaseModel, Field
+from pydantic import BaseModel, ConfigDict, UUID4, Field
 from datetime import datetime
 
 
 class BaseSchema(BaseModel):
-    class Config:
-        extra = 'forbid'
-        from_attributes = True
+    model_config = ConfigDict(
+        extra='forbid',         # Não permite campos extras nos modelos
+        from_attributes=True    # Permite a conversão de modelos SQLAlchemy para Pydantic
+    )
 
 
 class OutMixin(BaseSchema):
     id: Annotated[UUID4, Field(description='Identificador')]
-    created_at: Annotated[datetime, Field(description='Data de criação')]         
+    created_at: Annotated[datetime, Field(description='Data de criação')]
