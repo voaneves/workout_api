b6381bd532b37515bce730d177a2a6893abc646b
Victor Neves <victorneves478@gmail.com>
2025-08-21 12:10:47 -0300

    Update routers.py

--- workout_api/routers.py
+++ workout_api/routers.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from workout_api.atleta.controller import router as atleta
-from workout_api.categorias.controller import router as categorias
+from workout_api.categoria.controller import router as categoria
 from workout_api.centro_treinamento.controller import router as centro_treinamento
+from workout_api.auth.controller import router as auth
 
 api_router = APIRouter()
-api_router.include_router(atleta, prefix='/atletas', tags=['atletas'])
-api_router.include_router(categorias, prefix='/categorias', tags=['categorias'])
-api_router.include_router(centro_treinamento, prefix='/centros_treinamento', tags=['centros_treinamento'])
+api_router.include_router(auth, prefix='/auth')
+api_router.include_router(atleta, prefix='/atletas')
+api_router.include_router(categoria, prefix='/categorias')
+api_router.include_router(centro_treinamento, prefix='/centros_treinamento')
