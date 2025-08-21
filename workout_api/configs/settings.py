5ce4acab26b1124c3048b7d032cd8adabe30502d
Victor Neves <victorneves478@gmail.com>
2025-08-21 11:44:09 -0300

    Update settings.py

--- workout_api/configs/settings.py
+++ workout_api/configs/settings.py
@@ -1,8 +1,18 @@
-from pydantic import Field
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
-    DB_URL: str = Field(default='postgresql+asyncpg://workout:workout@localhost/workout')
+    """
+    Classe para gerenciar as configurações da aplicação.
+    Ela lê as variáveis de um arquivo .env.
+    """
+    model_config = SettingsConfigDict(
+        env_file='.env',   # Especifica o nome do arquivo de onde ler as variáveis
+        extra='ignore'     # Ignora variáveis extras que não foram definidas aqui
+    )
+
+    DATABASE_URL: str  # Define que a variável DATABASE_URL é esperada e deve ser uma string
 
 
+# Cria uma instância única das configurações para ser usada em todo o projeto
 settings = Settings()
